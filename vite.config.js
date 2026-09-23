edf780f40990c8cc79804d3e692a3aa32b4ecea3
laithalkhutaba <laithalkhutaba@example.com>
2026-09-24 01:24:10 +0300

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/workspace--6-/",
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
